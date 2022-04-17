087606401d08708989929cc19815b35506fd2c79
Nikita Semennikov <semennikovna@yandex.ru>
2022-04-17 17:13:57 +0300

    Created UI

---
+++ Controls practice/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Controls practice
+//
+//  Created by rezra on 17.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var orBut: UIButton!
+    @IBOutlet var swithes: [UISwitch]!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+    @IBAction func butPress() {
+    
+    }
+
+    @IBAction func switchTogglet(_ sender: UISwitch) {
+    }
+    
+    @IBAction func sliderMove() {
+    }
+    
+    @IBAction func textFieldEdited() {
+    }
+    
+}
+
