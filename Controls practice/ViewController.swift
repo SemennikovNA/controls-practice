4dd9defeffc182c88ca7732908b86264536a9387
Nikita Semennikov <semennikovna@yandex.ru>
2022-04-18 13:19:05 +0300

    Encremented number fo button, slider and text field

--- Controls practice/ViewController.swift
+++ Controls practice/ViewController.swift
@@ -14,24 +14,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var textField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
+    var number: UInt8 = 128 {
+        didSet {
+            updateUi()
+        }
     }
-
-
-    @IBAction func butPress() {
     
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUi()
+    }
+    
+    /// Updates all outlets to  number
+    
+    func updateUi() {
+        orBut.setTitle("\(number)", for: [])
+        //TODO: set swithes to number
+        slider.value = Float(number)
+        textField.text = "\(number)"
+    }
+ 
+    
+    @IBAction func butPress() {
+        number = UInt8((Int(number)+1) % 256)
+        print(#line, #function)
     }
 
     @IBAction func switchTogglet(_ sender: UISwitch) {
+        print(#line, #function, sender.tag)
     }
     
     @IBAction func sliderMove() {
+        number = UInt8(slider.value)
+        print(#line, #function)
     }
     
     @IBAction func textFieldEdited() {
+        number = UInt8(textField.text ?? "") ?? 128
+        print(#function)
     }
     
 }
